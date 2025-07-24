35e6761fdceb9fc3744420b4cc2929d48d3c8406
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-24 10:18:52 +0700

    update additem 2

--- getting-started-app/spec/routes/addItem.spec.js
+++ getting-started-app/spec/routes/addItem.spec.js
@@ -9,7 +9,7 @@ jest.mock("uuid", () => ({ v4: jest.fn() }));
 // mock method dùng để kiểm tra giá trị trả về
 // coi có phải là uuid hay không
 
-jest.mock("../../src/persistence", () => ({
+jest.mock("../../src/persistence/sqlite", () => ({
   removeItem: jest.fn(),
   storeItem: jest.fn(),
   getItem: jest.fn(),
@@ -18,28 +18,28 @@ jest.mock("../../src/persistence", () => ({
 test("check storeItem()", async () => {
   const id = "something-not-a-uuid";
   const name = "A sample item";
+
+  const req = { body: { name } };
+
+  const res = { send: jest.fn() };
+
+  // trả về id cố định
+  uuid.mockReturnValue(id);
+
+  await addItem(req, res);
+
+  const expectedItem = { id, name, completed: false };
+
+  // check result:
+  // 1. storeitem chỉ dc call 1 lần.
+  expect(db.storeItem.mock.calls.length).toBe(1);
+
+  // 2. storeitem phải kèm theo params
+  expect(db.storeItem.mock.calls[0][0]).toEqual(expectedItem);
+
+  // 3. response chỉ được gọi 1 lần
+  expect(res.send.mock.calls[0].length).toBe(1);
+
+  // 4. res.send phải trả về đúng expectedItem
+  expect(res.send.mock.calls[0][0]).toEqual(expectedItem);
 });
-
-const req = { body: { name } };
-
-const res = { send: jest.fn() };
-
-// trả về id cố định
-uuid.mockReturnValue(id);
-
-await addItem(req, res);
-
-const expectedItem = { id, name, completed: false };
-
-// check result:
-// 1. storeitem chỉ dc call 1 lần.
-expect(db.storeItem.mock.calls.length).toBe(1);
-
-// 2. storeitem phải kèm theo params
-expect(db.storeItem.mock.calls[0][0]).toEqual(expectedItem);
-
-// 3. response chỉ được gọi 1 lần
-expect(res.send.mock.calls[0].length).toBe(1);
-
-// 4. res.send phải trả về đúng expectedItem
-expect(res.send.mock.calls[0][0]).toEqual(expectedItem);
