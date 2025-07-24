4b079b3e0d10588f505bfd5caf8dad0e75c8161b
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-24 10:11:04 +0700

    update addItem

---
+++ getting-started-app/spec/routes/addItem.spec.js
@@ -0,0 +1,45 @@
+const db = require("../../src/persistence/sqlite");
+const addItem = require("../../src/routes/addItem");
+
+const ITEM = { id: 12345 };
+
+const { v4: uuid } = require("uuid");
+
+jest.mock("uuid", () => ({ v4: jest.fn() }));
+// mock method dùng để kiểm tra giá trị trả về
+// coi có phải là uuid hay không
+
+jest.mock("../../src/persistence", () => ({
+  removeItem: jest.fn(),
+  storeItem: jest.fn(),
+  getItem: jest.fn(),
+}));
+
+test("check storeItem()", async () => {
+  const id = "something-not-a-uuid";
+  const name = "A sample item";
+});
+
+const req = { body: { name } };
+
+const res = { send: jest.fn() };
+
+// trả về id cố định
+uuid.mockReturnValue(id);
+
+await addItem(req, res);
+
+const expectedItem = { id, name, completed: false };
+
+// check result:
+// 1. storeitem chỉ dc call 1 lần.
+expect(db.storeItem.mock.calls.length).toBe(1);
+
+// 2. storeitem phải kèm theo params
+expect(db.storeItem.mock.calls[0][0]).toEqual(expectedItem);
+
+// 3. response chỉ được gọi 1 lần
+expect(res.send.mock.calls[0].length).toBe(1);
+
+// 4. res.send phải trả về đúng expectedItem
+expect(res.send.mock.calls[0][0]).toEqual(expectedItem);
